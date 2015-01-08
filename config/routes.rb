3574ac9f33435fde7121f9682e684f68fa9391b5
Raul Brito <raul@LittleZ.local>
2015-01-08 15:18:43 +0100

    adding shopify integration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :accounts
+  resources :accounts do
+     member do
+       get 'test_connection'
+     end
+  end
 
   root 'dashboard#index'
   get 'dashboard/index'
